6b6e4f3e3cc270d9f0152a6dee98a37b6499732a
mahroch-ir <mahroch.ir@gmail.com>
2025-10-20 17:30:00 -0700

    Add files via upload

---
+++ AddTool.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddTool = () => {
+  return <div>Add Tool Component</div>;
+  };
+
+  export default AddTool;
---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import AddTool from "./pages/AddTool";
+
+const App = () => {
+  return (
+      <Router>
+            <div style={{ padding: "20px" }}>
+                    <nav style={{ marginBottom: "20px", display: "flex", gap: "20px" }}>
+                              <Link to="/">Dashboard</Link>
+                                        <Link to="/add-tool">Add Tool</Link>
+                                                </nav>
+
+                                                        <Routes>
+                                                                  <Route path="/" element={<Dashboard />} />
+                                                                            <Route path="/add-tool" element={<AddTool />} />
+                                                                                    </Routes>
+                                                                                          </div>
+                                                                                              </Router>
+                                                                                                );
+                                                                                                };
+
+                                                                                                export default App;
